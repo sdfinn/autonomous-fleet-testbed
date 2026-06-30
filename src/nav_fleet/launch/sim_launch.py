876740373a18b84d43218abfbc152ab80cc1fc0d
Mike <sdfinn70@gmail.com>
2026-06-29 17:36:39 -0700

    fix(session-10): add Apache 2.0 header to sim_launch.py

--- src/nav_fleet/launch/sim_launch.py
+++ src/nav_fleet/launch/sim_launch.py
@@ -1,3 +1,16 @@
+# Copyright 2026 Mike
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
 """Launch Gazebo simulation with the ugv_pt robot. Nav2 wired in Session 10.
 
 Path resolution uses pathlib.Path(__file__) instead of get_package_share_directory
